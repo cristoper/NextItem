527e1f2a718b02b1a082d192587231fb337ebed4
A. Cynic <chris@mretc.net>
2017-07-19 22:53:32 -0600

    Make Items array a property of the view controller
    That is tidier than leaving it as a global variable outside of the
    class.
    
    In large programs it is generally a good idea to avoid global variables
    for a few reasons: 1) they can be accessed from anywhere, making it more
    difficult to understand a program, 2) they discourage modular
    programming by allowing code to rely on other code in a haphazard
    fashion, making future maintenance of a program much more difficult, and
    3) in a multi-threaded program, they must be handled very carefully or
    they risk being accessed simultaneously by more than one thread
    thereby introducing difficult-to-find non-deterministic bugs.

--- NextItem/ViewController.swift
+++ NextItem/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let Items = [
+        "Call someone from Contacts, someone you haven't spoken with in the last month", "Drop down and give me 20 pushups and 20 sit-ups, now", "Motion is lotion: do ten minutes of PT, right now", "Motion is lotion: do ten minutes of PT, right now", "Run 1 fast mile", "Run 30 minute intervals", "Run a 5k", "Run a 5k", "Run a 5k", "Run a 10k ", "Run a 10k ", "Run 10 miles", "Read New Mexico history", "Browse a copy of The Rio Grande Sun", "Write 200 words in novel", "Write 400 words in novel", "Respond to a waiting email", "Work on 505 yard", "Hit the prayer closet for 15 minutes", "Sing a joyful song", "Sing a sad song", "Take a walk through the hood", "Drink a liter (of water)", "Drink a liter (of water)", "Eat some veggies", "Eat some veggies", "Read Kindle for an hour", "Read current hard copy book(s) for an hour", "Dust / vacuum / mop / windows / deck 505", "Dust / vacuum / mop / windows / deck 505", "Listen to Spanish lesson", "Listen to Spanish lesson", "Play 1/2 hour guitar (consider Sólo le pido and Jesu, Joy of Man's Desire)", "Play 1/2 hour guitar (consider Sólo le pido and Jesu, Joy of Man's Desire)", "Record guitar song for the Great Collection WIP"]
+    
     //LB: this is where I add the IBOutlet (from http://www.richardhsu.me/posts/2015/01/17/textview-border.html)
     @IBOutlet weak var TextViewItem: UITextView!
 
@@ -42,6 +45,3 @@ class ViewController: UIViewController {
         
         }
     }
-
-var Items = [
-    "Call someone from Contacts, someone you haven't spoken with in the last month", "Drop down and give me 20 pushups and 20 sit-ups, now", "Motion is lotion: do ten minutes of PT, right now", "Motion is lotion: do ten minutes of PT, right now", "Run 1 fast mile", "Run 30 minute intervals", "Run a 5k", "Run a 5k", "Run a 5k", "Run a 10k ", "Run a 10k ", "Run 10 miles", "Read New Mexico history", "Browse a copy of The Rio Grande Sun", "Write 200 words in novel", "Write 400 words in novel", "Respond to a waiting email", "Work on 505 yard", "Hit the prayer closet for 15 minutes", "Sing a joyful song", "Sing a sad song", "Take a walk through the hood", "Drink a liter (of water)", "Drink a liter (of water)", "Eat some veggies", "Eat some veggies", "Read Kindle for an hour", "Read current hard copy book(s) for an hour", "Dust / vacuum / mop / windows / deck 505", "Dust / vacuum / mop / windows / deck 505", "Listen to Spanish lesson", "Listen to Spanish lesson", "Play 1/2 hour guitar (consider Sólo le pido and Jesu, Joy of Man's Desire)", "Play 1/2 hour guitar (consider Sólo le pido and Jesu, Joy of Man's Desire)", "Record guitar song for the Great Collection WIP"]
